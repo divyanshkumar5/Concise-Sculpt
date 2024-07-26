fec365174d58bc7f14a807f9a1abba1a1799ddc0
Divyansh <94822471+divyanshkumar5@users.noreply.github.com>
2024-07-26 16:35:52 +0530

    data ingestion added

---
+++ main.py
@@ -0,0 +1,14 @@
+from ConciseSculpt.pipeline.stage_01_data_ingestion import DatalngestionTrainingPipeIine
+from ConciseSculpt.logging import logger
+
+
+
+STAGE_NAME = "Data Ingestion stage"
+try:
+    logger.info(f">>>>>>stage{STAGE_NAME}started<<<<<<")
+    data_ingestion = DatalngestionTrainingPipeIine()
+    data_ingestion.main()
+    logger.info(f">>>>>>stage{STAGE_NAME}completed<<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
---
+++ src/ConciseSculpt/pipeline/stage_01_data_ingestion.py
@@ -0,0 +1,18 @@
+from ConciseSculpt.config.configuration import ConfigurationManager
+from ConciseSculpt.components.data_ingestion import DataIngestion
+from ConciseSculpt.logging import logger 
+
+
+
+
+class DatalngestionTrainingPipeIine:
+    def __init__(self):
+        pass
+
+
+    def main(self):
+        config = ConfigurationManager()
+        data_ingestion_config = config.get_data_ingestion_config()
+        data_ingestion=DataIngestion(config=data_ingestion_config)
+        data_ingestion.download_file()
+        data_ingestion.extract_zip_file()
